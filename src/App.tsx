bfd059dc5ccd8b3fe108b048bbbc3f3d4be35930
Victor200524 <victor.donaire@hotmail.com>
2024-11-01 14:25:16 -0300

    Alteracao feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Navbar } from "./components/navbar/navbar"
+import { Navbar } from "./components/navbar/navbar";
 import { Home } from "./pages/home/home";
-import { CorpoDocentePages } from "./pages/corpo-docente/corpo-docente-pages";
+import { CorpoDocentePage } from "./pages/corpo-docente/corpo-docente-pages";
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/corpo-docente" element={<CorpoDocentePages/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/corpo-docente" element={<CorpoDocentePage />} />
           <Route path="/contato" element={<></>} />
           <Route path="/contact" element={<></>} />
         </Routes>
--- src/pages/corpo-docente/corpo-docente-pages.tsx
+++ src/pages/corpo-docente/corpo-docente-pages.tsx
@@ -1,47 +1,48 @@
 import { useEffect, useState } from "react"
-import { CorpoDocente } from "../../entities/corpo-docente";
+import { CorpoDocente } from "../../entities/corpo-docente"
 import { CardItem } from "./components/card-item/card-item";
 import { MainWrapper, ProfessorWrapper, Title, TitleWrapper } from "./corpo-docente-page.styles";
 
-export const CorpoDocentePages = () =>{
-    const [list,setList] = useState<CorpoDocente[]>([]);
+export const CorpoDocentePage = () => {
+    const [list, setList] = useState<CorpoDocente[]>([]);
 
-    useEffect(()=>{
-        fetch("https://infoeste-2024-api.vercel.app",{method:"get"})
+    useEffect(() => {
+        fetch("https://infoeste-2024-api.vercel.app", { method: "get"})
         .then((response) => response.json())
-        .then((value: { corpoDocente: CorpoDocente[]})=>{
+        .then((value: { corpoDocente: CorpoDocente[]}) => {
             setList(value.corpoDocente);
         });
-    },[]);
+    }, []);
 
-    const renderProfessorList = () =>
+    const renderProfessorList = () => 
         list
             .sort((a,b) => {
-                if(a.nome > b.nome)
+                if (a.nome > b.nome ) {
                     return 1;
-                if(a.nome < b.nome)
+                }
+                if (a.nome < b.nome ) {
                     return -1;
+                }
                 return 0;
-        })
-        .map((item) => {
-            <CardItem
-                professor = {{
-                    email: item.email,
-                    foto: item.graduacao,
-                    nome: item.nome,
-                    titulacao: item.titulacao
-                }}
-            />
-        });
+            })
+            .map((item) => (
+                <CardItem 
+                    professor={{
+                        email: item.email,
+                        foto: item.foto,
+                        graduacao: item.graduacao,
+                        nome: item.nome,
+                        titulacao: item.titulacao,
+                    }}
+                />
+            ));
 
-    return(
+    return (
         <MainWrapper>
             <TitleWrapper>
-                <Title>{"CORPO DOSCENTE"}</Title>
+                <Title>{"CORPO DOCENTE"}</Title>
             </TitleWrapper>
             <ProfessorWrapper>{renderProfessorList()}</ProfessorWrapper>
         </MainWrapper>
     );
-    
-
 }
